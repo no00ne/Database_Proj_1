672ad8e7e06298f9550f457960934afb7d9f2aaf
Steven <142069310+HeQijun12212131@users.noreply.github.com>
2023-10-29 21:33:22 +0800

    2.0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         try {
             DatabaseMani dm = new DataFactory().createDataMani("");
-            dm.readUser();
+            System.out.println("Connected");
+            Scanner in = new Scanner(System.in);
+            while(in.hasNext()) {
+                String text = in.nextLine();
+                long start = System.nanoTime(); // time spent
+                if (text.equalsIgnoreCase("readusers")) {
+                    dm.readUser();
+                } else if (text.equalsIgnoreCase("readcontents")) {
+                    dm.readContent();
+                } else if (text.equalsIgnoreCase("readvideos")) {
+                    dm.readVideo();
+                } else if (text.equalsIgnoreCase("exit")) {
+                    in.close();
+                    break;
+                } else {
+                    System.out.println("Command not found");
+                }
+                long end = System.nanoTime();
+                System.out.println("Time: " + (end - start) / 1000000 + "ms");
+            }
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
